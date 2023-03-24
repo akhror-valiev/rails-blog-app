ce91e2c41447c34eac082df12cfb1a31cc64a1eb
akhror-valiev <ahror.valiev@gmail.com>
2023-03-24 09:18:05 +0500

    Add methods and Create input data

--- db/migrate/20230323112215_rename_foreign_key_for_comments.rb
+++ db/migrate/20230323112215_rename_foreign_key_for_comments.rb
@@ -1,4 +1,4 @@
-class RenameColumnForComment < ActiveRecord::Migration[7.0]
+class RenameForeignKeyForComments < ActiveRecord::Migration[7.0]
   def change
     rename_column :comments, :user_id, :author_id
   end
---
+++ db/migrate/20230323112926_fix_column_name.rb
@@ -0,0 +1,13 @@
+class FixColumnName < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :users, :Name, :name
+    rename_column :users, :Photo, :photo
+    rename_column :users, :Bio, :bio
+    rename_column :users, :PostsCounter, :posts_counter
+    rename_column :posts, :Title, :title
+    rename_column :posts, :Text, :text
+    rename_column :posts, :LikesCounter, :likes_counter
+    rename_column :posts, :CommentsCounter, :comments_counter
+    rename_column :comments, :Text, :text
+  end
+end
