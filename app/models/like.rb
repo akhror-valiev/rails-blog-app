52864aafab9f1269d7edf961d8ea93c798b157de
akhror-valiev <ahror.valiev@gmail.com>
2023-03-24 10:47:50 +0500

    Update files

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+    belongs_to :post, counter_cache: true
+    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+    def update_likes_count
+        post.increment!(:likes_count)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ApplicationRecord
-  belongs_to :user
+    belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: true
+  	has_many :likes
+  	has_many :comments
+
+  	def update_posts_count
+  		  author.increment!(:posts_count)
+  	end
+
+  	def recent_comments
+  		  comments.order(created_at: :desc).limit(5)
+  	end
 end
