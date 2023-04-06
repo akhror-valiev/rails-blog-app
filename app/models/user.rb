fe685f974f110c7343b8827cfe196dbd8af49f60
akhror-valiev <ahror.valiev@gmail.com>
2023-04-06 19:25:51 +0500

    Add devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,20 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'author_id'
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
+  before_validation :set_counter
 
   validates :name, presence: true
   validates :posts_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def set_counter
+    self.posts_count = 0 if posts_count.nil?
+  end
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
